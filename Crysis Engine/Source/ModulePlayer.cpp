7795cbfb630cc0053428e3f1a6841fa19180726b
LordUnicorn31 <37652360+LordUnicorn31@users.noreply.github.com>
2021-08-08 18:17:16 +0200

    hi

---
+++ Crysis Engine/Source/ModulePlayer.cpp
@@ -0,0 +1,79 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModulePlayer.h"
+#include "Primitive.h"
+
+
+ModulePlayer::ModulePlayer(Application* app, bool start_enabled) : Module(app, start_enabled), vehicle(NULL)
+{
+	turn = acceleration = brake = 0.0f;
+}
+
+ModulePlayer::~ModulePlayer()
+{}
+
+// Load assets
+bool ModulePlayer::Start()
+{
+	LOG("Loading player");
+
+
+
+
+	// Don't change anything below this line ------------------
+
+	
+	vec3 direction(0,-1,0);
+	vec3 axis(-1,0,0);
+	
+
+
+	
+	return true;
+}
+
+// Unload assets
+bool ModulePlayer::CleanUp()
+{
+	LOG("Unloading player");
+
+	return true;
+}
+
+// Update: draw background
+update_status ModulePlayer::Update(float dt)
+{
+	turn = acceleration = brake = 0.0f;
+
+	if(App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+	{
+		acceleration = MAX_ACCELERATION;
+	}
+
+	if(App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+	{
+		if(turn < TURN_DEGREES)
+			turn +=  TURN_DEGREES;
+	}
+
+	if(App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+	{
+		if(turn > -TURN_DEGREES)
+			turn -= TURN_DEGREES;
+	}
+
+	if(App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+	{
+		brake = BRAKE_POWER;
+	}
+
+
+	char title[80];
+
+	App->window->SetTitle(title);
+
+	return UPDATE_CONTINUE;
+}
+
+
+
